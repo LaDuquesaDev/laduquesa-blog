51b06e588d8118edf1957d59abba6115d7173bc5
duquesa <laduquesadev@gmail.com>
2025-03-20 23:40:18 -0500

    update social media component

---
+++ src/models/rrss.ts
@@ -0,0 +1,7 @@
+export const socialLinks = [
+    { name: "LinkedIn", icon: "linkedin", url: "https://linkedin.com/in/laduquesadev/" },
+    { name: "GitHub", icon: "github", url: "https://github.com/LaDuquesaDev" },
+    { name: "Instagram", icon: "instagram", url: "https://instagram.com/laduquesadev/" },
+    { name: "YouTube", icon: "youtube", url: "https://youtube.com/@laduquesadev" },
+    { name: "Twitch", icon: "twitch", url: "https://twitch.tv/laduquesadev" }
+  ];
